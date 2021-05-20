5be16d444e497c3ee3e11fcfadaf0b07b0af1f35
Piero Mamberti <piero.mamberti@gmail.com>
2021-05-20 19:35:36 +0100

    Use httprouter instead

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module lenslocked.com
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(
+	w http.ResponseWriter,
+	r *http.Request,
+	_ httprouter.Params,
+) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>This is my Homepage</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(
+	w http.ResponseWriter,
+	r *http.Request,
+	ps httprouter.Params,
+) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(
 		w,
@@ -20,9 +28,22 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	)
 }
 
+func notFound(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(
+		w,
+		"<h2>We could not find the page!</h2>",
+	)
+}
+
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
+	r := httprouter.New()
+	r.GET("/", home)
+	r.GET("/contact", contact)
+	r.NotFound = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
