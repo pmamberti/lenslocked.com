b657b991d9c66215797eae64aa203885c28da2d7
Piero Mamberti <piero.mamberti@gmail.com>
2021-05-22 14:32:36 +0100

    Add Contacts view

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -19,15 +21,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(
-		w,
-		"To get in touch, please email to <a href=\"mailto:contact@example.com\">contact@example.com</a>.",
-	)
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	var err error
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
 	if err != nil {
 		panic(err)
 	}
