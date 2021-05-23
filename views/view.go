f0646921019479bc3f9e5bf4bd20f682e1414355
Piero Mamberti <piero.mamberti@gmail.com>
2021-05-23 17:45:53 +0100

    Glob layout files

--- views/view.go
+++ views/view.go
@@ -2,15 +2,18 @@ package views
 
 import (
 	"html/template"
+	"log"
+	"path/filepath"
+)
+
+const (
+	LayoutDir   = "views/layouts/"
+	TemplateExt = ".gohtml"
 )
 
 func NewView(layout string, files ...string) *View {
 	files = append(
-		files,
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml",
-		"views/layouts/footer.gohtml",
-	)
+		files, layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -26,4 +29,15 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+// layoutFiles returns a slice of strings representing
+// the layout files used in our app
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return files
 }
