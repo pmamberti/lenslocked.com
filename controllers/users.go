7077aa8cab93552595d8039b387307b7f75dfbc7
Piero Mamberti <piero.mamberti@gmail.com>
2021-05-25 18:44:39 +0100

    Move ParseForm to helpers.go

--- controllers/users.go
+++ controllers/users.go
@@ -5,8 +5,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/schema"
-
 	"lenslocked.com/views"
 )
 
@@ -43,16 +41,9 @@ type SignupForm struct {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		log.Fatal(err)
-	}
-
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := ParseForm(r, &form); err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Fprintln(w, r.PostForm["email"])
-	// fmt.Fprintln(w, r.PostForm["password"])
 	fmt.Fprintln(w, form)
 }
