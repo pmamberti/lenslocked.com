8b51661e04c8dac5409d07f1af8e989bbee67d9e
Piero Mamberti <piero.mamberti@gmail.com>
2021-05-30 12:05:31 +0100

    Add user model

--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.16
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/schema v1.2.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/lib/pq v1.10.2 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 )
---
+++ models/users.go
@@ -0,0 +1,62 @@
+package models
+
+import (
+	"errors"
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// ErrNotFound is returned when a resource cannot be found in
+// the database.
+var ErrNotFound = errors.New("models: resource not found")
+
+type UserService struct {
+	db *gorm.DB
+}
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+// Close closes the UserService DB connection
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+// DestructiveReset drops the user table and rebuilds it
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
+}
+
+// ByID will lookup the user by the ID provided
+// 1 - user, nil
+// 2 - nil, ErrNotFound
+// 3 - nil, otherError
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index`
+}
