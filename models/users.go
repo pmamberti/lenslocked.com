46537e7a2e2a0912edc20fb3f98d73d970424b7c
Piero Mamberti <piero.mamberti@gmail.com>
2021-05-30 13:09:46 +0100

    Add Update and Delete method for UserService

--- models/users.go
+++ models/users.go
@@ -8,9 +8,13 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-// ErrNotFound is returned when a resource cannot be found in
-// the database.
-var ErrNotFound = errors.New("models: resource not found")
+var (
+	// ErrNotFound is returned when a resource cannot be found in
+	// the database.
+	ErrNotFound = errors.New("models: resource not found")
+	// Err InvalidID returns when an invalid ID is provided.
+	ErrInvalidID = errors.New("models: ID provided is invalid")
+)
 
 type UserService struct {
 	db *gorm.DB
@@ -25,6 +29,17 @@ func NewUserService(connectionInfo string) (*UserService, error) {
 	return &UserService{
 		db: db,
 	}, nil
+}
+
+// first will query using the provided gorm.DB and it wwill get
+// the first item returned, place it into dst. If nothing
+// is found, it will return ErrNotFound.
+func first(db *gorm.DB, dst interface{}) error {
+	err := db.First(dst).Error
+	if err == gorm.ErrRecordNotFound {
+		return ErrNotFound
+	}
+	return err
 }
 
 // Close closes the UserService DB connection
@@ -44,15 +59,46 @@ func (us *UserService) DestructiveReset() {
 // 3 - nil, otherError
 func (us *UserService) ByID(id uint) (*User, error) {
 	var user User
-	err := us.db.Where("id = ?", id).First(&user).Error
-	switch err {
-	case nil:
-		return &user, nil
-	case gorm.ErrRecordNotFound:
-		return nil, ErrNotFound
-	default:
-		return nil, err
+	db := us.db.Where("id = ?", id)
+	err := first(db, &user)
+	return &user, err
+}
+
+// ByEmail will lookup the user by the Email address provided
+// 1 - user, nil
+// 2 - nil, ErrNotFound
+// 3 - nil, otherError
+
+func (us *UserService) ByEmail(email string) (*User, error) {
+	var user User
+	db := us.db.Where("email = ?", email)
+	err := first(db, &user)
+	return &user, err
+}
+
+// Create creates the provided user and the backfill data
+// like ID, CreatedAt and UpdatedAt fields.
+func (us *UserService) Create(user *User) error {
+	return us.db.Create(user).Error
+}
+
+// Update will update the proviced user with all the data
+// in the provided user object.
+func (us *UserService) Update(user *User) error {
+	return us.db.Save(user).Error
+}
+
+// Delete will delete the proviced user, selected by ID.
+func (us *UserService) Delete(id uint) error {
+	if id == 0 {
+		return ErrInvalidID
 	}
+
+	user := User{
+		Model: gorm.Model{ID: id},
+	}
+
+	return us.db.Delete(&user).Error
 }
 
 type User struct {
