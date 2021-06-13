1e4e68d234080a0c36ccdfde018a0ab0d506e174
Piero Mamberti <piero.mamberti@gmail.com>
2021-06-13 21:03:20 +0100

    Set Remember on Create()

--- models/users.go
+++ models/users.go
@@ -133,17 +133,11 @@ func (uv *userValidator) ByRemember(token string) (*User, error) {
 }
 
 func (uv *userValidator) Create(user *User) error {
-	if user.Remember == "" {
-		token, err := rand.RememberToken()
-		if err != nil {
-			return err
-		}
-		user.Remember = token
-	}
-
-	if err := runUserValFuncs(user,
+	err := runUserValFuncs(user,
 		uv.bcryptPassword,
-		uv.hmacRemember); err != nil {
+		uv.setRememberIfUnset,
+		uv.hmacRemember)
+	if err != nil {
 		return err
 	}
 
@@ -203,6 +197,19 @@ func (uv *userValidator) hmacRemember(user *User) error {
 		return nil
 	}
 	user.RememberHash = uv.hmac.Hash(user.Remember)
+	return nil
+}
+
+func (uv *userValidator) setRememberIfUnset(user *User) error {
+	if user.Remember != "" {
+		return nil
+	}
+
+	token, err := rand.RememberToken()
+	if err != nil {
+		return err
+	}
+	user.Remember = token
 	return nil
 }
 
