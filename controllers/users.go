4b71f1c7e20d40c5b7e61f7dc2a322c6827fdf64
Piero Mamberti <piero.mamberti@gmail.com>
2021-06-19 16:13:44 +0100

    Improve Error Handling for Login action

--- controllers/users.go
+++ controllers/users.go
@@ -82,27 +82,31 @@ type LoginForm struct {
 // Login is used to verify the provided credentials and log the user
 // in if they are correct.
 func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+	vd := views.Data{}
 	form := LoginForm{}
 	if err := ParseForm(r, &form); err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		vd.SetAlert(err)
+		u.LoginView.Render(w, vd)
+		return
 	}
 
 	user, err := u.us.Authenticate(form.Email, form.Password)
 	if err != nil {
 		switch err {
 		case models.ErrNotFound:
-			fmt.Fprintln(w, "Invalid email address provided")
-		case models.ErrPasswordIncorrect:
-			fmt.Fprintln(w, "Invalid password provided")
+			vd.AlertError("Invalid Email Address")
 		default:
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			vd.SetAlert(err)
 		}
+		u.LoginView.Render(w, vd)
 		return
 	}
 
 	err = u.signIn(w, user)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		vd.SetAlert(err)
+		u.LoginView.Render(w, vd)
 		return
 	}
 	http.Redirect(w, r, "/cookietest", http.StatusFound)
