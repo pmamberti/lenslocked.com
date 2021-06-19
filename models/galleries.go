9ba439622b872a7e6ac185ee825d5987ff0d3094
Piero Mamberti <piero.mamberti@gmail.com>
2021-06-19 17:17:42 +0100

    Add Services type and Gallery service

--- models/galleries.go
+++ models/galleries.go
@@ -1,10 +1,24 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+)
 
 // Gallery represents a gallery resource
 type Gallery struct {
 	gorm.Model
 	UserID uint   `gorm:"not_null;index"`
 	Title  string `gorm:"not_null"`
+}
+
+type GalleryService interface {
+	GalleryDB
+}
+
+type GalleryDB interface {
+	Create(gallery *Gallery) error
+}
+
+type galleryGorm struct {
+	db *gorm.DB
 }
