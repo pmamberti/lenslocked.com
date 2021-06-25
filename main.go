b695bbaa17a774f8ad24ac388a6b1f6e9aefce58
Piero Mamberti <piero.mamberti@gmail.com>
2021-06-25 10:15:20 +0100

    Create Gallery controller and New action

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func main() {
 
 	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers(services.User)
+	galleriesC := controllers.NewGalleries(services.Gallery)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
@@ -45,6 +46,10 @@ func main() {
 	r.Handle("/login", usersC.LoginView).Methods("GET")
 	r.HandleFunc("/login", usersC.Login).Methods("POST")
 	r.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
+
+	// Gallery routes
+	r.Handle("/galleries/new", galleriesC.New).Methods("GET")
+
 	fmt.Println("Starting server on http://localhost:3000 ...")
 	http.ListenAndServe(":3000", r)
 }
